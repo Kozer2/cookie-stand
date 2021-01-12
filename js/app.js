adcc0e736c7f1aa51031059580c204b9ec287b07
Kozer2 <benhemann@yahoo.com>
2021-01-12 15:30:34 -0600

    Started work on tables, created header and footer

--- js/app.js
+++ js/app.js
@@ -10,14 +10,14 @@ var opHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4
 //     totalCookie: 0,
 //     custPerHour: [],
 
-function Shop(name, minCust, maxCust, avgCookie, cookiePerHour, totalCookie, custPerHour){
+function Shop(name, minCust, maxCust, avgCookie){
     this.name = name,
     this.minCust = minCust,
     this.maxCust = maxCust,
     this.avgCookie = avgCookie,
-    this.cookiePerHour = cookiePerHour,
-    this.totalCookie = totalCookie,
-    this.custPerHour = custPerHour
+    this.cookiePerHour = [],
+    this.totalCookie = 0,
+    this.custPerHour = []
     
 };
 
@@ -48,3 +48,58 @@ Shop.prototype.cookiesAnHour = function(){
         console.log('The total sold is', this.totalCookie);
     }
 };
+
+
+// creater the header function to add the store hours to the table 
+function makeHeader(){
+    // create table
+    var saleTab = document.getElementById('salesContainer');
+    var hoursRow = document.createElement('tr');
+    var tableHeader = document.createElement('th');
+    tableHeader.textContent = 'Store Locations';
+    hoursRow.appendChild(tableHeader);
+    saleTab.appendChild(hoursRow);
+
+    for(var i = 0; i < opHours.length; i++){
+      tableHeader = document.createElement('th'); 
+      tableHeader.textContent = opHours[i];
+      hoursRow.appendChild(tableHeader);
+    }
+    tableHeader.textContent = 'Daily Location Total';
+    hoursRow.appendChild(tableHeader);
+};
+
+// create the foot function for hourly totals 
+function makeFooter(){
+    var foot = document.getElementById('salesContainer');
+    var footRow = document.createElement('tr');
+    var footFooter = document.createElement('th');
+    footFooter.textContent = 'Hourly Totals';
+    footRow.appendChild(footFooter);
+    foot.appendChild(footRow);
+};
+
+Shop.prototype.fillTable = function(){
+    
+    var hourlyCooks = document.createElement('tr');
+    var hourlyTotes = document.createElement('th');
+
+
+};
+
+makeHeader();
+makeFooter();
+
+
+
+
+
+
+
+
+
+
+
+
+
+new Shop('seattle', 23, 65, 6.3).cookiesAnHour();
